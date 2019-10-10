89e85d441bb9204902b39a262e200208e7c1bb0d
Xenosgrilda <dualmeidalima@gmail.com>
2019-10-09 21:17:50 -0300

    Suport for multiple combobox

--- app.js
+++ app.js
@@ -1,52 +1,63 @@
-const dropdownBtn = document.querySelector('.nice-input-dropdown-button');
-const icon = document.querySelector('.nice-input-dropdown-button > i');
-const input = document.querySelector('#nice-input');
-const datalist = document.querySelector('.datalist');
+// UI
+const wrapperElements = document.querySelectorAll('.nice-input-container');
 
-dropdownBtn.addEventListener('click', () => {
-    datalist.classList.toggle('shown');
-    icon.classList.toggle('rotate-180');
-})
+// LISTENERS
+wrapperElements.forEach(wrapper => {
 
-// Filtering and Displaying ComboBox
-input.addEventListener('input', filterList);
-input.addEventListener('dblclick', filterList);
+    const dropdownBtn = wrapper.querySelector('.nice-input-dropdown-button');
+    const input = wrapper.querySelector('.nice-input');
 
-// Put value into input
-datalist.querySelectorAll('ul > li').forEach(item => {
-    item.addEventListener('click', () => {
-
-        input.value = item.textContent;
-
-        if((datalist.classList.contains('shown'))){
-            datalist.classList.remove('shown');
-        }
-    })
-})
-
-// Closing ComboBox
-window.addEventListener('click', () => {
-
-    if( (event.target != datalist)      && 
-        (event.target != dropdownBtn)   && 
-        (event.target != icon)          &&
-        (event.target != input)) {
-
-        if((datalist.classList.contains('shown'))){
-            datalist.classList.remove('shown');
-        }
-        if((icon.classList.contains('rotate-180'))){
-            icon.classList.remove('rotate-180');
-        }
-    }
+    // Show dropdown
+    dropdownBtn.addEventListener('click', showDatalist);
+    // Filter input
+    input.addEventListener('input', filterList);
+    input.addEventListener('dblclick', filterList);
+    // Assign <li> value to input
+    wrapper.querySelectorAll('ul > li').forEach(li => {
+        li.addEventListener('click', getValueFromLi);
+    });
 });
 
-function filterList() {
+window.addEventListener('click', closeDataList);
+
+// Functions
+function findDatalistParent(element){
+
+    if(element.parentElement){
+        if(element.parentElement.classList.contains('nice-input-container')){
+            return element.parentElement;
+        } else if(element.tagName === 'BODY' || element.tagName === 'HTML') {
+            return null;
+        } else {
+            return findDatalistParent(element.parentElement);
+        }
+    } else {
+        return null;
+    }
+}
+
+function showDatalist(event){
+
+    const wrapper = findDatalistParent(event.currentTarget);
+    const datalist = wrapper.querySelector('.datalist');
+    const icon = wrapper.querySelector('.nice-input-dropdown-button > i');
+
+    datalist.classList.toggle('shown');
+    icon.classList.toggle('rotate-180');
+};
+
+function filterList(event) {
 
     let found = false;
 
+    // Local UI Elements
+    const wrapper = findDatalistParent(event.currentTarget);
+    const datalist = wrapper.querySelector('.datalist');
+    const icon = wrapper.querySelector('.nice-input-dropdown-button > i');
+    const input = wrapper.querySelector('.nice-input');
+
     // Removing message item
-    const messageItem = document.querySelector('#message-list-item');
+    const messageItem = wrapper.querySelector('#message-list-item');
     messageItem !== null ? messageItem.remove() : null;
 
     // Second dblclick to close
@@ -84,5 +95,50 @@ function filterList() {
         li.id = 'message-list-item';
 
         datalist.querySelector('ul').appendChild(li);
+    }
+}
+
+function getValueFromLi(event){
+    
+    const wrapper = findDatalistParent(event.currentTarget);
+    const datalist = wrapper.querySelector('.datalist');
+    const icon = wrapper.querySelector('.nice-input-dropdown-button > i');
+    const li = event.currentTarget;
+    const input = wrapper.querySelector('.nice-input');
+
+    if(wrapper !== null) {
+
+        input.value = li.textContent;
+
+        if((datalist.classList.contains('shown'))){
+            datalist.classList.remove('shown')
+            icon.classList.remove('rotate-180');
+        }
+    } else {
+        console.error('Couldn\'t find wrapper element to get children');
+        return false;
+    };
+}
+
+function closeDataList(){
+
+    const parent = findDatalistParent(event.target);
+
+    if (parent === null) {
+
+        wrapperElements.forEach(wrapper => {
+            
+            const datalist = wrapper.querySelector('.datalist');
+            const icon = wrapper.querySelector('.nice-input-dropdown-button > i');
+
+            if((datalist.classList.contains('shown'))){
+                datalist.classList.remove('shown');
+            }
+            if((icon.classList.contains('rotate-180'))){
+                icon.classList.remove('rotate-180');
+            }
+        })
+    } else { 
+        return false;
     }
 }
